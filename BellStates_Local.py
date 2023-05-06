0869321c83be04bb6ef3523637cf39e50d65c88c
Federico <fissore.federico2001@gmail.com>
2023-05-06 21:32:11 +0200

    Updated demos

---
+++ BellStates_Local.py
@@ -0,0 +1,12 @@
+from LocalUtils import *
+
+qreg_q = QuantumRegister(2, 'q')
+creg_c = ClassicalRegister(2, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.h(qreg_q[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+
+Utils.plotCountBars(LocalUtils.simulate(circuit))
